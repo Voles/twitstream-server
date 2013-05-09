019cfe373c86bcd7cac8ebf099b53e972b4bdb62
Voles <niels.dequeker@gmail.com>
2013-05-09 21:37:40 +0200

    Implemented set keyword functionality + prevent error messages from being emitted

--- app.js
+++ app.js
@@ -7,29 +7,92 @@ var fs = require('fs')
 // twitter api connection
 var t = new twitter({
 	consumer_key: '',
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secret: ''
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 });
+
+// keywords
+var keyword = 'abc';
+var clientCount = 0;
+var activeStream = null;
+var io = null;
 
 // server
-var server = http.createServer().listen(8080, function()
+var serverio = http.createServer().listen(8080, function()
 {
     console.log('Listening at: http://localhost:8080');
+
+    // start socket.io
+    io = socketio.listen(serverio);
+    io.sockets.on('connection', function (socket)
+    {
+        clientCount++;
+        connectedClientsUpdate();
+
+        socket.on('set:keyword', function(data)
+        {
+            console.log('Got set new keyword request!!!');
+            updateKeyword(data);
+
+            socket.broadcast.emit('get:keyword', keyword);
+        });
+
+        socket.on('disconnect', function ()
+        {
+            clientCount--;
+            connectedClientsUpdate();
+        });
+    });
 });
 
-// socket.io
-socketio.listen(server).on('connection', function (socket)
+function connectedClientsUpdate()
 {
-	// open twitter stream
-	t.stream('statuses/filter', {track: ['javascript']},
-        function(stream)
+    if (clientCount > 0)
+    {
+        console.log('Start stream. Clients: ' + clientCount);
+        twitterStream = t.stream(
+            'statuses/filter', 
+            {track: ['#' + keyword]},
+            function (stream)
+            {
+                activeStream = stream;
+                stream.on('data', function (tweet)
+                {
+                    sendTweet(tweet);
+                });
+            }
+        );
+    }
+    else
+    {
+        console.log('Stop stream. Clients: ' + clientCount);
+        activeStream.destroy();
+    }
+}
+
+function updateKeyword(new_keyword)
+{
+    keyword = new_keyword;
+
+    if (activeStream)
+    {
+        activeStream.destroy();
+        connectedClientsUpdate();
+    }
+}
+
+/**
+* Send tweet
+*/
+function sendTweet(tweet)
+{
+    // send tweet to the connected client
+    if (io.sockets)
+    {
+        if (!tweet.disconnect)
         {
-			stream.on('data', function(tweet)
-			{
-				// send tweet to the connected client
-				socket.emit('tweet', tweet);
-			});
+            io.sockets.emit('tweet', tweet);
         }
-    );
-});
+    }
+}
